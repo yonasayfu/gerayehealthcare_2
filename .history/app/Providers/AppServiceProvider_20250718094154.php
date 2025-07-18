789f8000a3cd2f0c844d2449fbebca8e57c7c852
yonasayfu <yonasayfu28@gmail.com>
2025-07-18 09:42:38 +0300

    seeder add 22

--- .history/app/Providers/AppServiceProvider_20250718094154.php
+++ .history/app/Providers/AppServiceProvider_20250718094154.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
     }
 }
