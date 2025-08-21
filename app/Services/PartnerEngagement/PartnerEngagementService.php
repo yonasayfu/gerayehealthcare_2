2fff4d126ead8dc6475bce1cb93d4cc4757f4f92
yonasayfu <yonasayfu28@gmail.com>
2025-08-21 22:52:25 +0300

    engagemetn done

--- app/Services/PartnerEngagement/PartnerEngagementService.php
+++ app/Services/PartnerEngagement/PartnerEngagementService.php
@@ -18,8 +18,22 @@ class PartnerEngagementService extends BaseService
 
     protected function applySearch($query, $search)
     {
-        return $query->where('engagement_type', 'like', "%{$search}%")
-            ->orWhere('summary', 'like', "%{$search}%");
+        return $query->where(function ($q) use ($search) {
+            // Base fields
+            $q->where('engagement_type', 'ilike', "%{$search}%")
+              ->orWhere('summary', 'ilike', "%{$search}%");
+
+            // Related Partner name
+            $q->orWhereHas('partner', function ($qp) use ($search) {
+                $qp->where('name', 'ilike', "%{$search}%");
+            });
+
+            // Related Staff first/last name
+            $q->orWhereHas('staff', function ($qs) use ($search) {
+                $qs->where('first_name', 'ilike', "%{$search}%")
+                   ->orWhere('last_name', 'ilike', "%{$search}%");
+            });
+        });
     }
 
     protected function getExportConfig(): array
